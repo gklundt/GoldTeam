41e4798af1fdbe3b7931958aec7d7b2448d2b98c
Gordon Klundt <gklundt@gmail.com>
2017-01-21 12:20:47 -0600

    Committed more changes.  And some more.  This is an important feature.

--- src/goldteam/GoldTeam.java
+++ src/goldteam/GoldTeam.java
@@ -8,5 +8,6 @@ public class GoldTeam {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // I'm going to make some more changes.
+        // I can do this all day.  For a year.
     }
 }
