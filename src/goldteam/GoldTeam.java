a0655eec660f2a349b313c87df94a819f6fce0f3
Gordon Klundt <gklundt@gmail.com>
2017-01-20 22:22:22 -0600

    Committing. and updated something.

--- src/goldteam/GoldTeam.java
+++ src/goldteam/GoldTeam.java
@@ -18,14 +18,6 @@ public class GoldTeam {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Making Changes");
-        System.out.println("Making More Changes");
-        // branched from dev.
-        
     }
     
 }
