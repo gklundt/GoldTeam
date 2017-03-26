682a51a5ad484e1ab9c292a1d040166b434643f5
Gordon Klundt <gklundt@gmail.com>
2017-03-26 08:13:03 -0500

    Added Removable interface to game object.

--- src/goldteam/domain/GameObject.java
+++ src/goldteam/domain/GameObject.java
@@ -3,30 +3,43 @@ package goldteam.domain;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.Polygon;
-import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public abstract class GameObject {
+public abstract class GameObject implements Removable {
 
     protected Point positionVector;
     protected Polygon shape;
     protected Image image;
     protected GameEngine gamedata;
+    protected final ArrayList<ActionListener> removableListeners;
 
     public GameObject(GameEngine gamedata, Point initialPoint) {
         this.gamedata = gamedata;
         this.gamedata.addGraphicsUpdateTimerListener(l -> GraphicsUpdateHandler());
         this.gamedata.addEffectsTimerListener(l -> UpdateEffectHandler());
         this.gamedata.addMapUpdateTimerListener(l -> MapUpdateTimerHandler());
+        this.removableListeners = new ArrayList<>();
         this.positionVector = initialPoint;
     }
 
     protected abstract void Update();
-    
+
     protected abstract void GraphicsUpdateHandler();
 
     protected abstract void UpdateEffectHandler();
-    
+
     protected abstract void MapUpdateTimerHandler();
+
+    @Override
+    public void addRemovableListener(ActionListener listener) {
+        this.removableListeners.add(listener);
+    }
+
+    @Override
+    public void removeRemovableListener(ActionListener listener) {
+        this.removableListeners.remove(listener);
+    }
 
     public Point PositionVector() {
         return this.positionVector;
---
+++ src/goldteam/domain/Removable.java
@@ -0,0 +1,11 @@
+package goldteam.domain;
+
+import java.awt.event.ActionListener;
+
+public interface Removable {
+
+    public void addRemovableListener(ActionListener listener);
+
+    public void removeRemovableListener(ActionListener listener);
+
+}
