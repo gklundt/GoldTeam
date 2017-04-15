321e523e395cc30cd6b0cab1556af3887a918fa2
Gordon Klundt <gklundt@gmail.com>
2017-04-15 18:16:36 -0500

    Map Math Complete

--- src/goldteam/panels/TestCollidersPanel.java
+++ src/goldteam/panels/TestCollidersPanel.java
@@ -160,7 +160,6 @@ public class TestCollidersPanel extends GamePanelBase
         this.gameObjectBuilder = new FlyerEnemyBuilder(gameData, this); //Passed so Flyers can create Launchers
         this.addGameObject(gameObjectProvider.build(gameObjectBuilder, new Point(400, 500)));
         
-        
         //Little platform
         this.platformBuilder = new PitPlatformBuilder(gameData);
         addGameObject(this.platformProvider.build(platformBuilder, new Point(310, 775), 10, 10));
